f056d7224dbcb0c425579f05b66e93dbe54ac9f8
Konstantinos Ninas <104201293+Ninas2@users.noreply.github.com>
2023-02-01 17:49:58 +0200

    Update streamlit_app.py
    Added Menu Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Menu')
+streamlit.text('Chicken Tika Masalla')
+streamlit.text('Moussaka')
+streamlit.text('Greek Salad')
